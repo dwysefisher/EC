3f759ddd9499e1c14cfbf6c0b0957c11f70bab0b
Dustin Wyse-Fisher <dwysefisher@eureka.edu>
2014-01-30 07:35:10 -0600

    general updates

---
+++ back-up/secondary-page-headers.php
@@ -0,0 +1,82 @@
+<?php if ( 'academics_page' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/academics-header.jpg" class="secondary-header-img" alt="Eureka College - Academics" />
+<?php elseif ( 'admissions_page' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/admissions-header.jpg" class="secondary-header-img" alt="Eureka College - Admissions" />
+<?php elseif ( 'alumni_page' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/alumni-header.jpg" class="secondary-header-img" alt="Eureka College - Alumni" />
+<?php elseif ( 'arts_page' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/arts-header.jpg" class="secondary-header-img" alt="Eureka College - Arts" />
+<?php elseif ( 'athletics_page' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/athletics-header.jpg" class="secondary-header-img" alt="Eureka College - Athletics" />
+<?php elseif ( 'career_service' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/career-services-header.jpg" class="secondary-header-img" alt="Eureka College - Career Services" />
+<?php elseif ( 'discover_page' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/discover-header.jpg" class="secondary-header-img" alt="Eureka College - Discover" />
+<?php elseif ( 'faculty_page' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/faculty-header.jpg" class="secondary-header-img" alt="Eureka College - Faculty" />
+<?php elseif ( 'giving_page' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/giving-header.jpg" class="secondary-header-img" alt="Eureka College - Giving" />
+<?php elseif ( 'library_page' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/library-header.jpg" class="secondary-header-img" alt="Eureka College - Library" />
+<?php elseif ( 'on_campus_page' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/oncampus-header.jpg" class="secondary-header-img" alt="Eureka College - On Campus" />
+<?php elseif ( 'parents_page' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/parents-header.jpg" class="secondary-header-img" alt="Eureka College - Parents" />
+<?php elseif ( 'reagan_page' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/reagan-header.jpg" class="secondary-header-img" alt="Eureka College - Reagan" />
+<?php elseif ( 'staff_page' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/staff-header.jpg" class="secondary-header-img" alt="Eureka College - Staff" />
+<?php elseif ( 'students_page' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/student-header.jpg" class="secondary-header-img" alt="Eureka College - Students" />
+<?php elseif ( 'student_life_page' == get_post_type() ) :?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/studentlife-header.jpg" class="secondary-header-img" alt="Eureka College - Student Life" />
+<?php elseif ( 'visitors_page' == get_post_type() ) : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/visitors-header.jpg" class="secondary-header-img" alt="Eureka College - Visitors" />
+<?php else : ?>
+	<img src="<?php echo get_template_directory_uri(); ?>/library/images/discover-header.jpg" class="secondary-header-img" alt="Eureka College" />
+<?php elseif ( is_page('5706')) : ?>
+<img src="<?php echo get_template_directory_uri(); ?>/library/images/titleix-header.jpg" class="secondary-header-img” alt=”Eureka College - Title IX" />
+
+<?php endif; ?>
+
+
+<table border="0" cellpadding="0" cellspacing="0" width="100%" id="textEdit">
+  <tbody>
+    <tr>
+      <td valign="top" align="left" styleclass=" style_Content style_MainText" style="font-size: 12pt; font-family: Arial, Helvetica, sans-serif; color: #4c4c4c; line-height: 20pt; padding: 7px 12px;">
+        <div>
+          <p style="margin-top: 0px; margin-bottom: 0px;">This annual conference brings together an elite group of alumni and students who desire to build connections with and for their alma mater.</p>
+          <p style="margin-top: 0px; margin-bottom: 0px;">&nbsp;</p>
+          <p style="margin-top: 0px; margin-bottom: 0px;">See the <a shape="rect" href="http://www.eureka.edu/alumni/leadership-conference-details/">full schedule</a>.</p>
+          <a target="_blank" track="on" linktype="1" style="text-decoration: underline;" href="http://www.eureka.edu/alumni/leadership-conference/#schedule">Read more</a> about our line up of speakers.&nbsp;<br><br>The conference is free to all EC alumni, thanks to the generous sponsorship of a friend and supporter of EC alumni initiatives.<br><br>
+          <h4>During the conference, you will -</h4>
+          <ul>
+            <li>Build your personal network and inspire connections with student leaders</li>
+            <li>Sharpen your skills with sessions on marketing yourself, mentoring others and managing your life</li>
+            <li>Recharge your batteries with fellow alumni and delicious complimentary food and drink</li>
+          </ul>
+          <p style="margin-top: 0px; margin-bottom: 0px;">This event runs concurrently with the Student Leadership Conference, offering the opportunity to mingle with students and later break into informational sessions that will challenge you and connect you with the Eureka Community. Your life will be enriched as you learn, connect and grow as a person and as a professional.</p>
+          <h4><span style="color: #990033;">Alumni Track Friday Evening (Business Attire)</span></h4>
+          <ul style="list-style-type: none;">
+            <li><b>6-6:30 pm:</b> Roundtable Prep for Student Leaders: Lou Ann Stambaugh Hayes '88</li>
+            <li><b>6-7 pm:</b> Alumni Registration</li>
+            <li><b>6:30 pm:</b> Roundtables with Student Leaders</li>
+            <li><b>7 pm:</b> Alumni/Senior Leaders Opening Reception</li>
+            <li><b>7:30 pm:</b> Opening Dinner with Alumni/Trustee Spotlight: Mickeisha Turner Armstrong '93</li>
+            <li><b>9 pm:</b> Dessert Reception with Alumni, Juniors and Seniors</li>
+          </ul>
+          <h4><span style="color: #990033;">Alumni Track Saturday (Eureka Gear Attire)</span></h4>
+          <ul style="list-style-type: none;">
+            <li><b>7:30 am:</b> Combined Deluxe Continental Breakfast</li>
+            <li><b>8 am:</b> Opening Session with Alumni spotlight: Anna Freidinger Palmer '07</li>
+            <li><b>9 am:</b> Managing Your Image through Identity Capital: Dr. Ann Fulop</li>
+            <li><b>10 am:</b> Influence Matters: Dina Lind Emser '76</li>
+            <li><b>11 am:</b> Managing your Mentoring Relationships: Inspiring Your Team to Greatness, Gary Urwiler '92</li>
+            <li><b>12 noon: </b>Combined Closing Luncheon: Managing Your Social Capital, Mrs. Nonnie Owens on Social Capital: The Power of Civility</li>
+          </ul>
+          <div style="color: #990033;"><strong>Questions? Call the Alumni Development Office at (309) 467-6139.</strong></div>
+        </div>
+      </td>
+    </tr>
+  </tbody>
+</table>
