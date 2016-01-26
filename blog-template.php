15fe52e1fa124877e6617ddb4cb8e19169808ba0
Dustin Wyse-Fisher <dwysefisher@eureka.edu>
2016-01-26 09:48:53 -0600

    further blog styling

---
+++ blog-template.php
@@ -0,0 +1,99 @@
+<html>
+
+	<head>
+		<link rel="stylesheet" type="text/css" href="/c/normalize.css">
+		<style>
+		body {margin: 2em; font-family: 'Merriweather', Georgia, serif; line-height: 200%; font-weight: 300;}
+
+		.blog-body {
+			width: 100%;
+			margin: 0 auto;
+		}
+
+		.blog-metadata {
+			width: 100%;
+			font-family: 'Open Sans', Arial, sans-serif;
+			color: gray;
+		}
+
+		.blog-metadata p {
+			font-size: 75%;
+			line-height: 125%;
+		}
+
+		.blog-subtitle{
+			color: #990033;
+			font-family: 'Open Sans', sans-serif;
+			font-weight: 300;
+			text-transform: uppercase;
+		}
+
+		a {
+			color: #990033;
+			text-decoration: none;
+			border-bottom: 1px dotted #990033;
+		}
+
+		a:hover {
+			color: #cc9900;
+		}
+
+		@media only screen and (min-width: 35em) {
+			.blog-body {
+				width: 66.6%;
+				float: right;
+			}
+
+			.blog-metadata {
+				width: 33.3%;
+				float: left;
+				margin-top: 5em;
+			}
+
+			.blog-metadata img {
+				float: right;
+				padding-right: 2em;
+			}
+		}
+
+		</style>
+	</head>
+
+	<body>
+
+		<article class="blog-metadata">
+		<img src="https://pbs.twimg.com/profile_images/3700182800/e5c70f38c8c2eb51898dd061e5b8d0d3.jpeg" style="width=125px; height: 150px;">
+
+<p>Published <?php echo date("F d, Y");?><br />
+		Chaplain Bruce Fowlkes</p>
+
+<p>Related Topics<br />
+	<a href="#">Student Life</a>
+	<a href="#">Category</a>
+
+	</article>
+
+		<article class="blog-body">
+		<h2 class="blog-subtitle">A holiday message for freshman and their families</h2>
+
+
+		<p>Make a list of all of your favorite things. On that list would be the most awesome comfort food, your top music cut by your favorite band of all time, the greatest movie, your most precious object, and the like. Add to that list your favorite activity, done with your best friend. Then add your fondest memory involving a precious loved one. What else would be on your all-time favorite list of things, experiences, memories, people and places?</p>
+
+		<p>What if you could only enjoy those things once a year? And here's my question: Would they become more or less enjoyable, more or less precious because you can only have them once a year?</p>
+
+		<p>That, for me, is the power of holiday traditions. Roasted turkey, if you think about it, is not that special, especially if you had it all the time. But baste it with fond memories, stuff it with a beloved aunt's secret recipe dressing that shows up&mdash;as she does&mdash;only once a year. Savor it over hilarious family yarns, and it becomes so much more than an ordinary fowl. Holiday music and decorations would hold much less magic to the ear and eye if always present, or if you worked in a year-round Christmas shop. Holiday traditions are precious because they are only once a year.</p>
+
+		<p>Some traditions are important, even when you've outgrown them. Do you have any of those? I remember my surprising sadness when the ugly yet all-important star that went on top of the tree was missing! I was in college, mind you, and I was disappointed that the pathetic, aluminum foil covered cardboard star, that I must have made in kindergarten, was gone from the family tree, replaced by my mother with a new store-bought, plug-in, sacrilege of a star! "Sorry, Brucey, I thought you out grew that ol' thing," she said.</p>
+
+		<p>Objects, spaces, sounds, smells, tastes, faces all powerfully accumulate meaning with time. Memories and feelings stick to holiday traditions like tinsel to footie pajamas. But becoming an adult, creating a new center of identity as a college student, can make going home for the holidays feel surprisingly different, strange, even wrong. Or sweeter.</p>
+
+		<p>You are a different person than you were before school started, and so is your family. So give your relationships with them a fresh start.</p>
+
+		<p>It might take a few days to slip back into the old routines, some good and some bad, but be assured of your ability to shape new ways of living and relating. If certain topics always bring arguments (and not the good kind) then make a promise to yourself to avoid the topic if possible. If certain situations always bring regret, then plan your alternative now.</p>
+
+		<p>And finally&mdash;be prepared&mdash;your old room might not be yours anymore. I know! If it has not been transformed into a sewing room or workout room or taken over by your little brother, then be prepared for your old room to look and feel different, even if nothing has been touched. You are different. That's ok. It really is.</p>
+
+		</article>
+
+	</body>
+</html>
